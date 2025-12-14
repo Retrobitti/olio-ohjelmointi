82cc5966e65788e0966e3c1904699083643c5b69
Retrobitti <PekkaParviainen@protonmail.com>
2025-12-14 19:32:10 +0200

    h6b

---
+++ h6b/main.cpp
@@ -0,0 +1,13 @@
+#include "exampleclass.h"
+#include <QCoreApplication>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    ExampleClass example;
+
+    example.startToWait();
+
+    return a.exec();
+}
